0e3653376ad57aab7532c6edf0fce2f471c95ee3
taejun0 <xownswns@naver.com>
2024-09-05 19:22:57 +0900

    Test

--- src/apis/instance.js
+++ src/apis/instance.js
@@ -12,20 +12,17 @@ export const instance = axios.create({
   timeout: 10000, // 10초 타임아웃 설정
 });
 
-// 요청 인터셉터 추가
 instance.interceptors.request.use(
-  (config) => config,
-  (error) => Promise.reject(error)
-);
-
-// 응답 인터셉터 추가
-instance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.code === "CUSTOM_ERRORCODE") {
-      console.log("Request timeout");
-      window.location.href = "/error"; // 타임아웃 시 에러 페이지로 리디렉션
+  (config) => {
+    const accessToken = Cookies.get('accessToken'); // 요청을 보낼 때마다 쿠키에서 액세스 토큰을 가져옵니다.
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`; // 액세스 토큰이 있으면 헤더에 추가합니다.
+      config.withCredentials = true;
     }
+
+    return config;
+  },
+  (error) => {
     return Promise.reject(error);
-  }
+  },
 );
