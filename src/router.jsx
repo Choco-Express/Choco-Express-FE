a457d8092c5fe00d5cac9e51f353065e2c76fddd
sayyyho <323psh@naver.com>
2024-09-08 19:10:22 +0900

    Chore: 테스트 파일 삭제

--- src/router.jsx
+++ src/router.jsx
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
         element: <NotFound />,
       },
       {
-        path: "/login",
+        path: "",
         element: <LoginPage />,
       },
       {
