3ebfc1b525f3be3b6daaeb48b6fcf2b0afe89a53
sayyyho <323psh@naver.com>
2024-09-23 16:29:52 +0900

    [Fix] #13 - 인스타 앱으로 이동 가능하게 추가

--- src/utils/shareInsta.js
+++ src/utils/shareInsta.js
@@ -3,30 +3,56 @@ import html2canvas from "html2canvas";
 
 export const shareInsta = () => {
   const element = document.getElementById("capture"); // 캡처할 부분을 감싼 div의 ID
+
   if (element) {
     // 캡처 전에 버튼을 숨기기
     const buttons = element.querySelectorAll("button");
     buttons.forEach((button) => (button.style.visibility = "hidden"));
 
-    html2canvas(element).then((canvas) => {
-      // 캡처된 이미지를 Data URI로 변환
-      const imageUri = canvas.toDataURL("image/png");
+    // 모든 이미지가 로드된 후 캡처
+    const images = element.querySelectorAll("img");
+    let loadedImages = 0;
 
-      // 다운로드 기능 (선택 사항)
-      const link = document.createElement("a");
-      link.download = "choco-express.png"; // 저장될 파일명
-      link.href = imageUri;
-      link.click();
-
-      // 캡처 후 버튼 다시 보이게
-      buttons.forEach((button) => (button.style.visibility = "visible"));
-
-      // 1초 대기 후 인스타그램으로 이동
-      setTimeout(() => {
-        redirectToInsta(imageUri);
-      }, 1000); // 1초 대기
+    images.forEach((img) => {
+      if (img.complete) {
+        loadedImages++;
+      } else {
+        img.onload = () => {
+          loadedImages++;
+          if (loadedImages === images.length) {
+            captureAndShare(element, buttons); // 캡처 함수 호출
+          }
+        };
+      }
     });
+
+    if (loadedImages === images.length) {
+      captureAndShare(element, buttons); // 캡처 함수 호출
+    }
   }
+};
+
+const captureAndShare = (element, buttons) => {
+  html2canvas(element, {
+    useCORS: true, // CORS 문제 해결을 위한 옵션
+  }).then((canvas) => {
+    // 캡처된 이미지를 Data URI로 변환
+    const imageUri = canvas.toDataURL("image/png");
+
+    // 다운로드 기능 (선택 사항)
+    const link = document.createElement("a");
+    link.download = "choco-express.png"; // 저장될 파일명
+    link.href = imageUri;
+    link.click();
+
+    // 캡처 후 버튼 다시 보이게
+    buttons.forEach((button) => (button.style.visibility = "visible"));
+
+    // 1초 대기 후 인스타그램으로 이동
+    setTimeout(() => {
+      redirectToInsta(imageUri);
+    }, 1000); // 1초 대기
+  });
 };
 
 // 캡처 완료 후 인스타그램으로 이동하는 콜백 함수
