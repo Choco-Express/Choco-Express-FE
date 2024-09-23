474c9d103f29893ef03d85d7217da50667b72fbb
sayyyho <323psh@naver.com>
2024-09-23 16:22:38 +0900

    [Fix] #13 - 인스타 앱으로 이동 가능하게 추가

--- src/utils/shareInsta.js
+++ src/utils/shareInsta.js
@@ -9,9 +9,13 @@ export const shareInsta = () => {
     buttons.forEach((button) => (button.style.visibility = "hidden"));
 
     html2canvas(element).then((canvas) => {
+      // 캡처된 이미지를 Data URI로 변환
+      const imageUri = canvas.toDataURL("image/png");
+
+      // 다운로드 기능 (선택 사항)
       const link = document.createElement("a");
       link.download = "choco-express.png"; // 저장될 파일명
-      link.href = canvas.toDataURL("image/png");
+      link.href = imageUri;
       link.click();
 
       // 캡처 후 버튼 다시 보이게
@@ -19,21 +23,23 @@ export const shareInsta = () => {
 
       // 1초 대기 후 인스타그램으로 이동
       setTimeout(() => {
-        redirectToInsta();
+        redirectToInsta(imageUri);
       }, 1000); // 1초 대기
     });
   }
 };
 
 // 캡처 완료 후 인스타그램으로 이동하는 콜백 함수
-const redirectToInsta = () => {
-  if (isIOS) {
-    window.location.replace("https://www.instagram.com/create/story");
-  } else if (isAndroid) {
-    window.location.replace(
-      "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end"
-    );
+const redirectToInsta = (imageUri) => {
+  const backgroundColor = "#ffffff"; // 예시로 흰색 배경
+  const link = "https://choco-express.site"; // 공유할 링크
+
+  if (isIOS || isAndroid) {
+    const storyUri = `instagram-stories://share?source_application=your.app.package&background_image=${imageUri}&background_top_color=${backgroundColor}&background_bottom_color=${backgroundColor}&content_url=${link}`;
+
+    window.location.href = storyUri;
   } else {
-    window.location.replace("https://www.instagram.com/");
+    // 인스타그램 웹으로 리디렉션
+    window.location.href = "https://www.instagram.com/";
   }
 };
