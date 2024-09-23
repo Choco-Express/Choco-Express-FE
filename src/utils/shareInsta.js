b5ba4853aee249d23408a20fad4fb9507c40a265
sayyyho <323psh@naver.com>
2024-09-23 15:59:10 +0900

    [Fix] #13 - 사진 다운 전 인스타 이동 방지추가

--- src/utils/shareInsta.js
+++ src/utils/shareInsta.js
@@ -17,16 +17,23 @@ export const shareInsta = () => {
       // 캡처 후 버튼 다시 보이게
       buttons.forEach((button) => (button.style.visibility = "visible"));
 
-      // 다운로드 후 기기별 인스타그램 스토리로 이동
-      if (isIOS) {
-        window.location.replace("https://www.instagram.com/create/story");
-      } else if (isAndroid) {
-        window.location.replace(
-          "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end"
-        );
-      } else {
-        window.location.replace("https://www.instagram.com/");
-      }
+      // 1초 대기 후 인스타그램으로 이동
+      setTimeout(() => {
+        redirectToInsta();
+      }, 1000); // 1초 대기
     });
+  }
+};
+
+// 캡처 완료 후 인스타그램으로 이동하는 콜백 함수
+const redirectToInsta = () => {
+  if (isIOS) {
+    window.location.replace("https://www.instagram.com/create/story");
+  } else if (isAndroid) {
+    window.location.replace(
+      "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end"
+    );
+  } else {
+    window.location.replace("https://www.instagram.com/");
   }
 };
